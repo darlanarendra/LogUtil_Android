19f5a81734641a18f3681fbd83f0f4b3e138bf9d
Narendra Darla <naren.darla@gmail.com>
2021-01-31 22:43:18 -0800

    Progress

--- temperatureconverter/src/main/java/com/bluewhales/bayarea/Logging.java
+++ temperatureconverter/src/main/java/com/bluewhales/bayarea/Logging.java
@@ -1,13 +1,12 @@
-package com.converter.temperatureconverter;
+package com.bluewhales.bayarea;
 
 import android.util.Log;
 
 public class Logging {
 
-
-    private static final String TAG = Logging.class.getSimpleName();
-
+    public static final String TAG = Logging.class.getSimpleName();
     public static void d(String message){
         Log.v(TAG, message);
     }
 }
+
