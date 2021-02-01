a46f18bf42234a700479dc75dd6cb6cdd85b01ba
Narendra Darla <naren.darla@gmail.com>
2021-01-31 18:04:39 -0800

    Progress

---
+++ example/src/main/java/com/willy/example/ListFragment.java
@@ -0,0 +1,29 @@
+package com.willy.example;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListFragment
+        extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new MyAdapter(getContext()));
+    }
+}
